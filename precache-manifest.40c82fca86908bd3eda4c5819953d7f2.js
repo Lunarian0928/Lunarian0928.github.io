f802943c8a5f740ccc0841e4d6128972e04e01c8
Lunarian0928 <74519471+Lunarian0928@users.noreply.github.com>
2022-07-04 19:08:36 +0900

    Add files via upload

---
+++ precache-manifest.40c82fca86908bd3eda4c5819953d7f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8dde10aecd9b7c95f4ca241c0e3440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ca7c754cdf9e03a870",
+    "url": "/static/css/main.163cb20a.chunk.css"
+  },
+  {
+    "revision": "164406aaad289d398d8a",
+    "url": "/static/js/2.014815e4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.014815e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ca7c754cdf9e03a870",
+    "url": "/static/js/main.df398615.chunk.js"
+  },
+  {
+    "revision": "bbb319e839c49d2e874a",
+    "url": "/static/js/runtime-main.aff97f00.js"
+  },
+  {
+    "revision": "f2fbdcb8306f500fe1ec2401a4509936",
+    "url": "/static/media/movit_cover.f2fbdcb8.png"
+  }
+]);
